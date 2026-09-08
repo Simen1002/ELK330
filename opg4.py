7e9babe2083559b58fabded446990af8cc5aef1e
PizzaBakeren24 <284015@uis.no>
2026-09-08 17:31:06 +0200

    oppgave 4 løst

--- opg4.py
+++ opg4.py
@@ -1,4 +1,4 @@
 import pandas as pd
-df = read_csv("load_data.csv", parse_dates=["Time(Local)"])
+df = pd.read_csv("load_data.csv", parse_dates=["Time(Local)"], index_col="Time(Local)")
 
-print(df.head)
+print(df.head())
