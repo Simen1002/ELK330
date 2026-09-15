a949603e81c2978a5b69e1d7e00637d40128e9d8
PizzaBakeren24 <284015@uis.no>
2026-09-15 12:48:08 +0200

    oppgave 9 løst

--- opg4.py
+++ opg4.py
@@ -6,6 +6,13 @@ df["Time(Local)"] = pd.to_datetime(df["Time(Local)"], format="mixed", utc = True
 df=df.set_index("Time(Local)")
 df["Netto"] = (df["Production"] - df["Consumption"])
 
+dogn = df.loc["2026-01-01"]
+dogn.plot()
+plt.xlabel("Tid")
+plt.ylabel("Effekt")
+plt.grid()
+plt.show()
+
 max_production = df["Production"].max()
 min_production = df["Production"].min()
 mean_production = df["Production"].mean()
@@ -13,9 +20,8 @@ mean_production = df["Production"].mean()
 max_netto = df["Netto"].max()
 min_netto = df["Netto"].min()
 
-dogn = df.loc["2026-01-01"]
-dogn.plot()
-plt.xlabel("Tid")
-plt.ylabel("Effekt")
-plt.grid()
-plt.show()  
+idx_max_netto = df["Netto"].idxmax()
+idx_min_netto = df["Netto"].idxmin()
+
+print(max_netto, idx_max_netto)
+print(min_netto, idx_min_netto)
