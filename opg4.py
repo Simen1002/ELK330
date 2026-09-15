02c2d73e958c9e8c6e18b27dde9ceeb02dcfe016
PizzaBakeren24 <284015@uis.no>
2026-09-15 11:24:14 +0200

    oppgave 7 løst

--- opg4.py
+++ opg4.py
@@ -1,4 +1,6 @@
 import pandas as pd
-df = pd.read_csv("load_data.csv", parse_dates=["Time(Local)"], index_col="Time(Local)")
+df = pd.read_csv("load_data.csv", parse_dates=["Time(Local)"], decimal=",", index_col="Time(Local)")
+
+df["Netto"] = (df["Production"] - df["Consumption"])
 
 print(df.head())
