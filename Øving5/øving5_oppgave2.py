5bb5f72eda8eeb0823f08f8a575e409f434292ad
PizzaBakeren24 <284015@uis.no>
2026-09-17 19:22:40 +0200

    Modellert alle komponentene. Trengs justering

--- Øving5/øving5_oppgave2.py
+++ Øving5/øving5_oppgave2.py
@@ -8,18 +8,40 @@ df["Time(Local)"] = pd.to_datetime(df["Time(Local)"], format="%d.%m.%Y %H:%M:%S 
 df=df.set_index("Time(Local)")
 df = df.sort_index()
 
+#Utlavgt døgn
 dogn = df.loc["2026-03-25", "Consumption"]
 
-l0 = 1
-Ai = 1
+#Modellen
+l0 = 15850
+
+#Night peak
+An = -350
+myn = 2.5    #tid på topp
+sigman = 1 #bredde
 t = np.arange(24)
-myi = 25
-sigmai = 30
+
+#Morning peak
+Am = 2500
+mym = 7
+sigmam = 3
+
+#Evening peak
+Ae = 2500
+mye = 19
+sigmae = 4.5
+
+night_peak =  An*np.exp(-(t - myn)**2 /(2*sigman**2))
+morning_peak = Am*np.exp(-(t - mym)**2 /(2*sigmam**2))
+evening_peak = Ae*np.exp(-(t - mye)**2 /(2*sigmae**2))
+
+load_model = l0 + night_peak + morning_peak + evening_peak
 
 #l_t = l0 + Ai*np.exp(-(t - myi)**2 /(2*sigmai**2))
 
 plt.plot(t, dogn.values, marker="o", linestyle="None")
-plt.xlabel("Tid i timer")
+plt.plot(t, load_model)
+plt.xlabel("Time")
 plt.ylabel("Forbruk")
+plt.xticks(np.arange(0, 24, 1))
 plt.grid()
 plt.show()
