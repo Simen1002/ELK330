94b34b03a51be25f6c5870160430b723666a53a8
PizzaBakeren24 <284015@uis.no>
2026-09-17 16:54:28 +0200

    Oppgave 1 gjort

--- Øving5/øving5.py
+++ Øving5/øving5.py
@@ -1,13 +1,15 @@
 import math
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 l0 = 1
 Ai = 1
 t = np.linspace(0, 100, 101)
 myi = 25
-sigmai = 20
+sigmai = 30
 
 l_t = l0 + Ai*np.exp(-(t - myi)**2 /(2*sigmai**2))
 
-plot(t, l_t)
+plt.plot(t, l_t)
+plt.grid()
+plt.show()
