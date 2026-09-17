44733a70c7db98e0d93286b63fa8af311c747d1a
PizzaBakeren24 <284015@uis.no>
2026-09-17 20:47:11 +0200

    Ferdig

--- Øving5/øving5_oppgave2.py
+++ Øving5/øving5_oppgave2.py
@@ -41,7 +41,7 @@ morning_peak = Am*np.exp(-(t - mym)**2 /(2*sigmam**2))
 midday_peak = Amid*np.exp(-(t - mymid)**2 /(2*sigmamid**2))
 evening_peak = Ae*np.exp(-(t - mye)**2 /(2*sigmae**2))
 
-load_model = l0 + night_peak + morning_peak  + evening_peak
+load_model = l0 + night_peak + morning_peak + midday_peak + evening_peak
 
 #l_t = l0 + Ai*np.exp(-(t - myi)**2 /(2*sigmai**2))
 
