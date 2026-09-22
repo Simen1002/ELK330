9c4bc6e3700ecf36957d3b173470265d879a48c4
PizzaBakeren24 <284015@uis.no>
2026-09-22 15:33:24 +0200

    Oppgave 1 ferdig

---
+++ øving6/øving6_1.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+t = np.linspace(0, 24, 500)
+
+A = 800 
+mu = 13
+sigma = 3.5
+
+G = A * np.exp(-(t - mu)**2 / (2 * sigma**2))
+
+plt.plot(t, G)
+plt.xlabel("Tid i timer")
+plt.ylabel("Innstråling [W/m^2]")
+plt.title("Døgnprofil for innstråling")
+plt.xticks(np.arange(0, 25, 1))
+plt.grid()
+plt.show()
